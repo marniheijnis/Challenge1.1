81e8e4c5eeeeb9c26cb31de53727aded23131d22
marniheijnis <58250084+marniheijnis@users.noreply.github.com>
2020-05-12 10:45:24 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var mySpaceship = {
+	
+	parameters: 'fuel',
+	environment: 'gravity',
+	metrics: 'gravity converter',
+	isInOrder: true,
+
+	supplies1: {
+		water: 6,
+		food: 10,
+	},
+	supplies2: {
+		chocolate: 4,
+		cola: 25,
+	}
+};
+
+
+console.log(mySpaceship.fuel);
+
+console.log(mySpaceship.supplies1.chocolate);
+
+
+
+
+
+
+
