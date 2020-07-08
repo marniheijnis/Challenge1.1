2382be029e90edbd7be9a9f64d48c1e2df086949
marniheijnis <58250084+marniheijnis@users.noreply.github.com>
2020-07-08 21:12:05 +0200

    added buttons
    3 new buttons to change chocolate mode, normal mode and get an alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,20 @@
-var spaceship, text, sLen, i;
-spaceship = ["Chocolate", "Water", "Fuel"];
-sLen = spaceship.length;
 
-text = "<ul>";
-for (i = 0; i < sLen; i++) {
-  text += "<li>" + spaceship[i] + "</li>";
+var shipSpace = {
+  drink:"Water",
+  combustibles:"fuel",
+  passengers:1500,
+};
+
+document.getElementById("infoSpaceship").innerHTML = shipSpace.drink +' and '+ shipSpace.combustibles + ' ' +"are important, but they all love to eat chocolate Mars bars in space the most.";
+
+
+function chocolateMode() {
+  document.body.style.backgroundImage = "url('https://www.worldofsnacks.com/uploads/1/3/1/2/13127180/s643667121527569464_p29_i3_w640.jpeg')";
+
+function normal() {
+	document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/1745055.jpg')";
 }
-text += "</ul>";
+
 
 
 
